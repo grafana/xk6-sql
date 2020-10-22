ebc92b46b866a55e80a88e7527ea6723a1a305a5
Ivan Mirić <ivan@loadimpact.com>
2020-10-22 10:16:44 +0200

    Remove some unnecessary files for the xk6 plugin system

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# This fetches the k6-plugin-sql from GitHub and
-# builds it in your $GOPATH/src/github.com/mostafa/k6-plugin-sql
-
-go build -buildmode=plugin -ldflags="-s -w" -o sql.so github.com/mostafa/k6-plugin-sql
