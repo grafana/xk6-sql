b23d4684b25a5597a19149b31469caff0c7d17d9
Ivan Mirić <ivan@loadimpact.com>
2020-11-09 16:45:18 +0100

    Update test.js with xk6 example

--- test.js
+++ test.js
@@ -1,24 +1,23 @@
-/*
+import sql from 'k6/x/sql';
 
-This is a k6 test script that imports the k6-plugin-sql.
+const db = sql.open("sqlite3", "./test.db");
 
-*/
+export function setup() {
+  db.exec(`CREATE TABLE IF NOT EXISTS keyvalues (
+           id integer PRIMARY KEY AUTOINCREMENT,
+           key varchar NOT NULL,
+           value varchar);`);
+}
 
-import { check } from 'k6';
-import { open, query } from 'k6-plugin/sql';  // import sql plugin
+export function teardown() {
+  db.close();
+}
 
 export default function () {
-    let db = open("sqlite3", "./test.sqlite");
-    db.exec(`CREATE TABLE IF NOT EXISTS keyvalues (
-        id integer PRIMARY KEY AUTOINCREMENT,
-        key varchar NOT NULL,
-        value varchar);`);
-    db.exec("INSERT INTO keyvalues (key, value) VALUES('plugin-name', 'k6-plugin-sql');");
-    let results = query(db, "SELECT * FROM keyvalues;");
-    results.forEach(row => {
-        check(row, {
-            "correct key/value": r => r['key'] == 'plugin-name' && r['value'] == 'k6-plugin-sql'
-        });
-    });
-    db.close();
+  db.exec("INSERT INTO keyvalues (key, value) VALUES('plugin-name', 'k6-plugin-sql');");
+
+  let results = sql.query(db, "SELECT * FROM keyvalues;");
+  for (const row of results) {
+    console.log(`key: ${row.key}, value: ${row.value}`);
+  }
 }
