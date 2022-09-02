3c612414b06b6c63afbb79c66ee8e25745694f9d
Stefan Nedelchev <88819267+stefnedelchev@users.noreply.github.com>
2022-09-02 11:25:47 +0300

    Create oracle_test.js

---
+++ tests/oracle_test.js
@@ -0,0 +1,29 @@
+import sql from 'k6/x/sql';
+
+// The second argument is a Oracle connection string, e.g.
+// myuser:mypass@tcp(127.0.0.1:3306)/mydb
+const db = sql.open('oracle', '');
+
+export function setup() {
+  db.exec(`
+    CREATE TABLE IF NOT EXISTS keyvalues (
+      id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+      \`key\` VARCHAR(50) NOT NULL,
+      value VARCHAR(50) NULL
+    );
+  `);
+}
+
+export function teardown() {
+  db.close();
+}
+
+export default function () {
+  db.exec("INSERT INTO keyvalues (`key`, value) VALUES('plugin-name', 'k6-plugin-sql');");
+
+  let results = sql.query(db, "SELECT * FROM keyvalues WHERE `key` = ?;", 'plugin-name');
+  for (const row of results) {
+    // Convert array of ASCII integers into strings. See https://github.com/grafana/xk6-sql/issues/12
+    console.log(`key: ${String.fromCharCode(...row.key)}, value: ${String.fromCharCode(...row.value)}`);
+  }
+}
