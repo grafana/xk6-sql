01fb2be45306bafe112af68cafdd838c0ef5ab1b
Iván SZKIBA <szkiba@users.noreply.github.com>
2024-10-16 11:35:27 +0200

    test: skip Testcontainers based tests on os other than linux

--- drivers/mysql/register_test.go
+++ drivers/mysql/register_test.go
@@ -3,6 +3,7 @@ package mysql
 import (
 	"context"
 	_ "embed"
+	"runtime"
 	"testing"
 
 	"github.com/grafana/xk6-sql/sqltest"
@@ -15,6 +16,14 @@ import (
 var script string
 
 func TestIntegration(t *testing.T) { //nolint:paralleltest
+	if testing.Short() {
+		t.Skip()
+	}
+
+	if runtime.GOOS != "linux" {
+		t.Skip("Works only on Linux (Testcontainers)")
+	}
+
 	ctx := context.Background()
 
 	ctr, err := mysql.Run(ctx, "mysql:8.0.36")
--- drivers/postgres/register_test.go
+++ drivers/postgres/register_test.go
@@ -3,6 +3,7 @@ package postgres
 import (
 	"context"
 	_ "embed"
+	"runtime"
 	"testing"
 
 	"github.com/grafana/xk6-sql/sqltest"
@@ -17,6 +18,14 @@ import (
 var script string
 
 func TestIntegration(t *testing.T) { //nolint:paralleltest
+	if testing.Short() {
+		t.Skip()
+	}
+
+	if runtime.GOOS != "linux" {
+		t.Skip("Works only on Linux (Testcontainers)")
+	}
+
 	ctx := context.Background()
 
 	ctr, err := postgres.Run(ctx, "docker.io/postgres:16-alpine",
